26c1681bf4fc6b175da1d5ab2f778382e73b6db2
Adrien Jourdier <adrien@quimeo.fr>
2017-04-06 11:52:07 +0200

    Add clearwater architecture

---
+++ clearwter/app/assets/javascripts/application.js
@@ -0,0 +1,14 @@
+# file: app/assets/javascripts/application.rb
+require 'opal' # Not necessary if you load Opal from a CDN
+require 'clearwater'
+
+class Layout
+  include Clearwater::Component
+
+  def render
+    h1('Hello, world!')
+  end
+end
+
+app = Clearwater::Application.new(component: Layout.new)
+app.call
