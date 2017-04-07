9e161f2e4a731b9ed2e1b52bfbdd921a03a84317
Adrien Jourdier <adrien@quimeo.fr>
2017-04-07 09:57:20 +0200

    Add platform list

--- inesita/app/components/platform.rb
+++ inesita/app/components/platform.rb
@@ -3,6 +3,17 @@ class Platform
 
   def render
     div class: 'jumbotron text-center' do
+      ul do
+        li do
+          'Test de li'
+        end
+        li do
+          'Test de li 2'
+        end
+        li do
+          'Test de li 3'
+        end
+      end
     end
   end
 end
