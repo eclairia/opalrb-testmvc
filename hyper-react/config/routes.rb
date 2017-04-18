c0fdfec23211e2ee0e672bbebcf069a533e10bdf
Adrien Jourdier <adrien@quimeo.fr>
2017-04-18 15:29:58 +0200

    Create home#helloworld route

--- hyper-react/config/routes.rb
+++ hyper-react/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/helloworld'
+  root 'home#helloworld'
 
   mount Hyperloop::Engine => '/hyperloop'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
